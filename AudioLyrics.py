7095435ba8badd81872caf7462c3a4f6a1efec00
LordGonk <petecmo@gmail.com>
2015-01-03 13:39:12 -0500

    Accepts text only sources + source code cleanup

--- AudioLyrics.py
+++ AudioLyrics.py
@@ -1,7 +1,7 @@
 '''
 Anki Clozed Audio Generator
 Copyright 2014 Peter Moran.
-Version 0.8.0
+Version 0.8.5
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -22,13 +22,15 @@ AnkiLPCG
 
 ###################################################################
 ''' Options/Parameters '''
-line_depth = 3                  # Number of lines to include on a full lyrics
-initial_Dir = ''                # Optional override for starting file directory in the file selector (defaults to desktop)
-custom_anki_location = ''
+line_depth = 3                  # Number of lines per card
+custom_initial_dir = ''         # Starting file directory in the file selector (defaults to desktop)
+custom_anki_location = ''       # Location of Anki.exe if not default
 anki_User = 'User 1'            # User name in Anki window title bar and/or in 'My Documents/Anki/'
-ffmpegLoc = "/ffmpeg/bin/ffmpeg.exe"
-saveCSV = True                  # Option to save a copy of the lyrics file imported to Anki
-catch_duplicate_cards = True    # If two cards share the same front and back text, only the first will be exported to Anki
+saveCSV = True                  # Saves a copy of the file used to import to Anki
+catch_duplicate_cards = True    # Prevents duplicate cards (same front and back text) from importing
+show_stanza_count = True        # If cards have the same front context due to repetitions in the text, a hint will be included on repetitions 2 and on
+song_leadtime = 3000            # (For audio) How much time to lead into the first lyric
+ffmpegLoc = "/ffmpeg/bin/ffmpeg.exe" # Location of ffmpeg
 ###################################################################
 
 import os
@@ -52,21 +54,141 @@ root.withdraw()
 
 # Set starting file directory for finder
 user = getpass.getuser()
-if initial_Dir == '':
-    initial_Dir = 'C:/Users/%s/Desktop' % user
+if custom_initial_dir == '':
+    custom_initial_dir = 'C:/Users/%s/Desktop' % user
 
-# Open audio and lyric files
-AudioSegment.converter = ffmpegLoc
-audioLoc = askopenfilename(title='Pick a song', filetypes=[('Audio', ('*.mp3', '.wav', '.wma', '.m4a', 'm4p', '.aac', '.ogg', '.oga', '.flac', '.3gp')), ('All formats', ('*.*'))], initialdir=initial_Dir)
-if not audioLoc: 
+# Startup
+print('''Anki-ClozeTool v8.0.0
+Copyright 2014-2025 Peter Moran
+---------------------------------
+For help and options, see README.md''')
+
+# Open audio and text/lyric files
+textLoc = askopenfilename(title='Pick text or song lyric file', filetypes=[('Text or Lyric File', ('*.txt', '*.lrc'))], initialdir=custom_initial_dir)
+if not textLoc: 
     sys.exit()
-lrcLoc = askopenfilename(title='Pick song lyrics', filetypes=[('Lyric File', '*.lrc'), ('Text File', ('*.txt'))], initialdir=initial_Dir)
-if not lrcLoc: 
-    sys.exit()
-audioInFormat = os.path.splitext(audioLoc)[1][1:]
-audio = AudioSegment.from_file(audioLoc, format=audioInFormat)
-lrc = open(lrcLoc)
-    
+textInFormat = os.path.splitext(textLoc)[1][1:]
+
+if textInFormat == 'lrc':
+    AudioSegment.converter = ffmpegLoc
+    audioLoc = askopenfilename(title='Pick a song', filetypes=[('Audio', ('*.mp3', '.wav', '.wma', '.m4a', 'm4p', '.aac', '.ogg', '.oga', '.flac', '.3gp')), ('All formats', ('*.*'))], initialdir=custom_initial_dir)
+    if not audioLoc: 
+        sys.exit()
+    audioInFormat = os.path.splitext(audioLoc)[1][1:]
+    audio = AudioSegment.from_file(audioLoc, format=audioInFormat)
+else:
+    audio = None
+text = open(textLoc)
+
+# Variables and method for reading 'ahead' of the current text/lyric
+def nextContent(file):
+    '''Returns the next non-blank line in the file'''
+    while True:
+        read = file.readline()
+        if read != '\n':
+            break
+    return read
+targetLine = nextContent(text).rstrip()
+nextLine = nextContent(text).rstrip()
+def pullLine():
+    global targetLine
+    global nextLine
+    retval = targetLine
+    targetLine = nextLine
+    nextLine = nextContent(text).rstrip()
+    return retval
+def peekNextLine():
+    return targetLine
+
+# Read / set metadata
+if audio:
+    artist, title, offset = 'Unknown Artist', 'Unknown Title', 0
+    while True:
+        fileLine = pullLine()
+        tag = fileLine[1:3]
+        if tag == 'ar':
+            artist = fileLine[5:-1]
+        if tag == 'ti':
+            title = fileLine[5:-1]
+        if tag == 'of':
+            offset = int(fileLine[9:-1])
+        if any(c in peekNextLine()[1:3] for c in '0123456789'):
+            break
+else:
+    title = os.path.splitext(textLoc)[0]
+
+class Lyric:
+    def __init__(self, line):
+        if audio:
+            self.milliStart = self.startTime(line[:10])
+            self.lyric = line[10:]
+            self.nextMilli = self.endTime()
+        else:
+            self.lyric = line
+    def startTime(self, timeStamp):
+        milli = 0
+        milli += int(timeStamp[1:3]) * 60000
+        milli += int(timeStamp[4:6]) * 1000
+        milli += int(timeStamp[7:9]) * 10
+        milli += offset
+        return milli
+    def endTime(self):
+        if nextLine == '':
+            return len(audio)
+        else:
+            return self.startTime(peekNextLine())
+    def __str__(self):
+        return self.lyric
+
+class Card:
+    def __init__(self):
+        titleLyrc = '' # Fake lyric for song startTime
+        if audio:
+            titleLyrc = Lyric('[00:00.00][Song Start] ' + title + ' - ' + artist)
+            songStart = titleLyrc.nextMilli - song_leadtime
+            if songStart < 0:
+                songStart = 0
+            titleLyrc.milliStart = songStart
+        else:
+            titleLyrc = '[Start] ' + title
+        self.lyrics = [titleLyrc]
+    def add(self, line):
+        if len(self.lyrics) < line_depth:
+            self.lyrics.append(Lyric(line))
+        else:
+            self.lyrics = self.lyrics[1:]
+            self.lyrics.append(Lyric(line))
+    def exportText(self):
+        preSound, postSound = '', ''
+        if audio:
+            preSound = '[sound:' + self.preAudioFile() + ']<br>'
+            postSound = '[sound:' + self.postAudioFile() + ']<br>'
+        # Get text for front of lyrics
+        frontText = ''
+        for line in self.lyrics[:-2]:
+            frontText += str(line) + '<br>'
+        frontText += str(self.lyrics[-2])
+        # Add front lyrics and then the lyrics for the back
+        return preSound + frontText + '<br><b style="color:blue">[...]</b>\t' + postSound + frontText + '<br>' + str(self.lyrics[-1])
+    def __str__(self):
+        text = ''
+        for line in self.lyrics[:-1]:
+            text += str(line)+'\n'
+        text += str(self.lyrics[-1])
+        return text
+    def startTime(self):
+        return self.lyrics[0].milliStart
+    def contextEndTime(self):
+        return self.lyrics[-1].milliStart
+    def cardEndTime(self):
+        return self.lyrics[-1].endTime()
+    def preAudioFile(self):
+        return artist + '_' + title + '_' + str(self.startTime()) + '-' + str(self.contextEndTime()) + '.mp3'
+    def postAudioFile(self):
+        return artist + '_' + title + '_' + str(self.contextEndTime()) + '-' + str(self.cardEndTime()) + '.mp3'
+    def textEquals(self, card2):
+        return str(self) == str(card2)
+
 def locate_anki_executable():
     if custom_anki_location:
         if os.path.exists(custom_anki_location):
@@ -96,162 +218,62 @@ def locate_anki_executable():
         return anki_location
     else:
         print("*****")
-        print("WARNING: LPCG could not locate your Anki executable and will not be able to")
+        print("WARNING: Anki-ClozeTool could not locate your Anki executable and will not be able to")
         print("automatically import the generated cloze deletions. To solve this problem,")
-        print("please see the \"Setting a Custom Anki Location\" section in the README.")
+        print("please set a Custom Anki Location")
         print("*****")
         return None
 
 def open_anki(ankipath, anki_file):
     call([ankipath, anki_file.name])
 
-# Variables and method for reading 'ahead' of the current lyric
-def nextContent(file):
-    '''Returns the next non-blank line in the file'''
-    while True:
-        read = file.readline()
-        if read != '\n':
-            break
-    return read
-targetLine = nextContent(lrc).rstrip()
-nextLine = nextContent(lrc).rstrip()
-def pullLine():
-    global targetLine
-    global nextLine
-    retval = targetLine
-    targetLine = nextLine
-    nextLine = nextContent(lrc).rstrip()
-    return retval
-def peekNextLine():
-    return targetLine
-
-# Read title, album, and artist
-artist, title, album, offset = 'Unknown Artist', 'Unknown Title', 'Unknown Album', 0
-while True:
-    fileLine = pullLine()
-    tag = fileLine[1:3]
-    if tag == 'ar':
-        artist = fileLine[5:-1]
-    if tag == 'ti':
-        title = fileLine[5:-1]
-    if tag == 'al':
-        album = fileLine[5:-1]
-    if tag == 'of':
-        offset = int(fileLine[9:-1])
-    if any(c in peekNextLine()[1:3] for c in '0123456789'):
-        break
-
-class StampLyrc:
-    """A simple class for storing a line of lyrics and 
-    the time stamp it starts and ends"""
-    def startTime(self, timeStamp):
-        milli = 0
-        milli += int(timeStamp[1:3]) * 60000
-        milli += int(timeStamp[4:6]) * 1000
-        milli += int(timeStamp[7:9]) * 10
-        milli += offset
-        return milli
-    def endTime(self):
-        if nextLine == '':
-            return len(audio)
-        else:
-            return self.startTime(peekNextLine())
-    def __init__(self, line):
-        """Reads a .lcr formatted string to pull out the millisecond time
-        stamp and the line of lyrics associated with it"""
-        self.milliStart = self.startTime(line[:10])
-        self.lyric = line[10:]
-        self.nextMilli = self.endTime()
-    def __str__(self):
-        return self.lyric
-
-class Card:
-    def __init__(self):
-        titleLryc = StampLyrc('[00:00.00][Song Start] ' + title + ' - ' + album)  # Fake lyric for song start
-        songStart = titleLryc.nextMilli - 3000
-        if songStart < 0:
-            songStart = 0
-        titleLryc.milliStart = songStart
-        self.lyrics = [titleLryc]
-    def add(self, line):
-        if len(self.lyrics) < line_depth:
-            self.lyrics.append(StampLyrc(line))
-        else:
-            self.lyrics = self.lyrics[1:]
-            self.lyrics.append(StampLyrc(line))
-    def start(self):
-        return self.lyrics[0].milliStart
-    def preEnd(self):
-        return self.lyrics[-1].milliStart
-    def end(self):
-        return self.lyrics[-1].endTime()
-    def preAudioFile(self):
-        return album + '_' + title + '_' + str(self.start()) + '-' + str(self.preEnd()) + '.mp3'
-    def postAudioFile(self):
-        return album + '_' + title + '_' + str(self.preEnd()) + '-' + str(self.end()) + '.mp3'
-    def text(self):
-        text = ''
-        for line in self.lyrics[:-1]:
-            text += str(line)+'\n'
-        text += str(self.lyrics[-1])
-        return text
-    def textEquals(self, card2):
-        return self.text() == card2.text()
-    def __str__(self):
-        preSound = '[sound:' + self.preAudioFile() + ']<br>'
-        postSound = '[sound:' + self.postAudioFile() + ']<br>'
-        # Get text for front of lyrics
-        frontText = ''
-        for line in self.lyrics[:-2]:
-            frontText += str(line) + '<br>'
-        frontText += str(self.lyrics[-2])
-        # Add front lyrics and then the lyrics for the back
-        return preSound + frontText + '<br><b style="color:blue">[...]</b>\t' + postSound + frontText + '<br>' + str(self.lyrics[-1])
-
 ankipath = locate_anki_executable()
 
 # Create a file to write to
 if saveCSV is True:
-    csv = open(album + ' - ' + title + '.txt', 'w')
+    if audio:
+        csv = open(artist + ' - ' + title + '_ClozeToolExport' + '.txt', 'w')
+    else:
+        csv = open(title + '_ClozeToolExport' + '.txt', 'w')
 else:
     try:
         csv = tempfile.NamedTemporaryFile('w', delete=False)
     except:
         print("*****")
-        print("Could not create a temporary file. Please ensure:")
-        print("- the permissions on your system's temporary folder are correct.")
+        print("Could not create a temporary file. Please ensure the")
+        print("permissions on your system's temporary folder are correct.")
         print("\nThe original error follows:")
         traceback.print_exc()
         print("*****")
         exit()
 ankiMediaDirectory = r'C:\Users\{}\Documents\Anki\{}\collection.media\\'.format(user, anki_User)
 
-# Read lyrics
+# Read text/lyrics
 card = Card()
 currFileLine = pullLine()
 pastCards = []
 while currFileLine != '':
     card.add(currFileLine)
     if not any(card.textEquals(pastCard) for pastCard in pastCards) or not catch_duplicate_cards:
-        pastCards.append(copy.copy(card))
-        csv.write(str(card))
+        pastCards.append(copy.copy(card))        
+        csv.write(card.exportText())
         csv.write('\n')
-        if not os.path.isfile(ankiMediaDirectory + card.preAudioFile()):
-            selection = audio[card.start():card.preEnd()]
-            selection.export(ankiMediaDirectory + card.preAudioFile(), format='mp3')
-        if not os.path.isfile(ankiMediaDirectory + card.postAudioFile()):
-            selection = audio[card.preEnd():card.end()]
-            selection.export(ankiMediaDirectory + card.postAudioFile(), format='mp3')
+        if audio:
+            if not os.path.isfile(ankiMediaDirectory + card.preAudioFile()):
+                selection = audio[card.startTime():card.contextEndTime()]
+                selection.export(ankiMediaDirectory + card.preAudioFile(), format='mp3')
+            if not os.path.isfile(ankiMediaDirectory + card.postAudioFile()):
+                selection = audio[card.contextEndTime():card.cardEndTime()]
+                selection.export(ankiMediaDirectory + card.postAudioFile(), format='mp3')
     currFileLine = pullLine()
 
 # Import file to Anki, if a location has been found.
 if ankipath:
     open_anki(ankipath, csv)
     print("\n* Anki is importing your file. *")
-    print("If Anki does not appear, please start Anki and open your profile if")
-    print("necessary, then try running LPCG again.")
+    print("If Anki import does not appear, restart Anki and")
+    print("try running Anki-ClozeTool again.")
 else:
-    print("\nDone! Now import the file %s into Anki.\n" % csv.name)
+    print("\nDone! Now manually import the file %s into Anki.\n" % csv.name)
     print("To avoid having to do this manually in the future, consider setting the path")
-    print("to Anki in the script file, as described in the \"Setting a Custom Anki Location\"")
-    print("section of the README.\n")
+    print("to Anki in the script file")
